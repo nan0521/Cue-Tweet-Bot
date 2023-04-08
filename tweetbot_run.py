06e78ba1fa86922d81fbee8ccfa7933a12de4f68
NAN(CPK) <112696843+nan0521@users.noreply.github.com>
2023-04-08 20:21:28 +0800

    tweetbot pythin file

---
+++ tweetbot_run.py
@@ -0,0 +1,39 @@
+import tweepy
+import requests
+import os
+import json
+import random
+
+# Twitter API keys and access tokens
+consumer_key = os.environ['TWITTER_CONSUMER_KEY']
+consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
+access_token = os.environ['TWITTER_ACCESS_TOKEN']
+access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+
+# Authenticate with Twitter API using environment variables
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret, access_token, access_token_secret)
+
+# Create a Tweepy API object
+api = tweepy.API(auth)
+
+with open('./CardsData.json', 'r', encoding='utf-8') as json_file:
+    jsondata = json.load(json_file)
+    num = random.randint(0, len(jsondata['Cards']))
+    card = jsondata['Cards'][num]
+
+giturl = "https://raw.githubusercontent.com/Cpk0521/CUECardsViewer/master/public/"
+
+image_urls = [f"{giturl}{card['image']['Normal']}", f"{giturl}{card['image']['Blooming']}"]
+media_ids = []
+for url in image_urls:
+    response = requests.get(url)
+    filename = 'temp.jpg'
+    with open(filename, 'wb') as f:
+        f.write(response.content)
+    media = api.media_upload(filename)
+    media_ids.append(media.media_id)
+
+
+# Create a Tweet
+tweet = f"{card['alias']}{card['heroine']}\ntweet Bot Running Test"
+api.update_status(status=tweet, media_ids=media_ids)

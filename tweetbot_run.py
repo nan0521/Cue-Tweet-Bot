7d6417895810ec323d36d6a0e64b4f9f8e1d743c
NAN(CPK) <112696843+nan0521@users.noreply.github.com>
2023-04-08 20:42:38 +0800

    Update tweetbot_run.py

--- tweetbot_run.py
+++ tweetbot_run.py
@@ -5,35 +5,39 @@ import json
 import random
 
 # Twitter API keys and access tokens
-consumer_key = os.environ.get('TWITTER_CONSUMER_KEY')
-consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET')
-access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
-access_token_secret = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
+consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
+access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
+access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
 
-# Authenticate with Twitter API using environment variables
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret, access_token, access_token_secret)
+if(not consumer_key or not consumer_secret or not access_token or not access_token_secret):
+    print('can not find the env')
+else :
+    # Authenticate with Twitter API using environment variables
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret, access_token, access_token_secret)
 
-# Create a Tweepy API object
-api = tweepy.API(auth)
+    # Create a Tweepy API object
+    api = tweepy.API(auth)
 
-with open('./CardsData.json', 'r', encoding='utf-8') as json_file:
-    jsondata = json.load(json_file)
-    num = random.randint(0, len(jsondata['Cards']))
-    card = jsondata['Cards'][num]
+    with open('./CardsData.json', 'r', encoding='utf-8') as json_file:
+        jsondata = json.load(json_file)
+        num = random.randint(0, len(jsondata['Cards']))
+        card = jsondata['Cards'][num]
 
-giturl = "https://raw.githubusercontent.com/Cpk0521/CUECardsViewer/master/public/"
+    giturl = "https://raw.githubusercontent.com/Cpk0521/CUECardsViewer/master/public/"
 
-image_urls = [f"{giturl}{card['image']['Normal']}", f"{giturl}{card['image']['Blooming']}"]
-media_ids = []
-for url in image_urls:
-    response = requests.get(url)
-    filename = 'temp.jpg'
-    with open(filename, 'wb') as f:
-        f.write(response.content)
-    media = api.media_upload(filename)
-    media_ids.append(media.media_id)
+    image_urls = [f"{giturl}{card['image']['Normal']}", f"{giturl}{card['image']['Blooming']}"]
+    media_ids = []
+    for url in image_urls:
+        response = requests.get(url)
+        filename = 'temp.jpg'
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+        media = api.media_upload(filename)
+        media_ids.append(media.media_id)
 
 
-# Create a Tweet
-tweet = f"{card['alias']}{card['heroine']}\ntweet Bot Running Test"
-api.update_status(status=tweet, media_ids=media_ids)
+    # Create a Tweet
+    tweet = f"{card['alias']}{card['heroine']}\ntweet Bot Running Test"
+    api.update_status(status=tweet, media_ids=media_ids)
+

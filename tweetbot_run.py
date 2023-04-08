cd633ee43e300e9d3f1aa84dbf589743deafaa30
NAN(CPK) <112696843+nan0521@users.noreply.github.com>
2023-04-08 20:32:57 +0800

    Update tweetbot_run.py

--- tweetbot_run.py
+++ tweetbot_run.py
@@ -5,10 +5,10 @@ import json
 import random
 
 # Twitter API keys and access tokens
-consumer_key = os.environ['TWITTER_CONSUMER_KEY']
-consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
-access_token = os.environ['TWITTER_ACCESS_TOKEN']
-access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+consumer_key = os.environ.get('TWITTER_CONSUMER_KEY')
+consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET')
+access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
+access_token_secret = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
 
 # Authenticate with Twitter API using environment variables
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret, access_token, access_token_secret)

e8cc95d86787c3a220a85b8fa6cb87e806747857
NAN(CPK) <112696843+nan0521@users.noreply.github.com>
2023-05-28 18:27:17 +0800

    fix 453 problem

--- tweetbot_run.py
+++ tweetbot_run.py
@@ -16,33 +16,31 @@ if(not consumer_key or not consumer_secret or not access_token or not access_tok
 else :
     # Authenticate with Twitter API using environment variables
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret, access_token, access_token_secret)
-
-    # Create a Tweepy API object
-    api = tweepy.API(auth)
+    # Create a Tweepy API v1.1 object
+    api_v1 = tweepy.API(auth = auth)
+    # Create a Tweepy API v2 object
+    api_v2  = tweepy.Client(access_token = access_token,
+                        access_token_secret = access_token_secret,
+                        consumer_key = consumer_key,
+                        consumer_secret = consumer_secret)
 
     with open('./CardsData.json', 'r', encoding='utf-8') as json_file:
         jsondata = json.load(json_file)
         num = random.randint(0, len(jsondata['Cards']))
         card = jsondata['Cards'][num]
 
-    giturl = "https://raw.githubusercontent.com/Cpk0521/CUECardsViewer/master/public/"
+        giturl = "https://raw.githubusercontent.com/Cpk0521/CUECardsViewer/master/public/"
 
-    image_urls = [f"{giturl}{card['image']['Normal']}"]
+        image_urls = [f"{giturl}{card['image']['Normal']}", f"{giturl}{card['image']['Blooming']}"]
+        media_ids = []
+        for url in image_urls:
+            response = requests.get(url)
+            filename = 'temp.jpg'
+            with open(filename, 'wb') as f:
+                f.write(response.content)
+            media = api_v1.media_upload(filename)
+            media_ids.append(media.media_id)
 
-    if(card['image']['Blooming']):
-        image_urls.append(f"{giturl}{card['image']['Blooming']}")
-    
-    media_ids = []
-    for url in image_urls:
-        response = requests.get(url)
-        filename = 'temp.jpg'
-        with open(filename, 'wb') as f:
-            f.write(response.content)
-        media = api.media_upload(filename)
-        media_ids.append(media.media_id)
-
-
-    # Create a Tweet
-    tweet = f"★{card['rarity']}{card['alias']}{card['heroine']}"
-    api.update_status(status=tweet, media_ids=media_ids)
+        tweet = f"★{card['rarity']}{card['alias']}{card['heroine']}"
+        res = api_v2.create_tweet(text = tweet, media_ids=media_ids)
 
